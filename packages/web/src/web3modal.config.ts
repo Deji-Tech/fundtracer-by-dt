97c686948e22f9102f135f6e3481f85d132b035f
DEJI-TECH <dejitech2@gmail.com>
2026-01-27 14:00:12 +0100

    try 1

--- packages/web/src/web3modal.config.ts
+++ packages/web/src/web3modal.config.ts
@@ -4,7 +4,7 @@ import { createAppKit } from '@reown/appkit/react';
 import { Ethers5Adapter } from '@reown/appkit-adapter-ethers5';
 import { linea } from '@reown/appkit/networks';
 
-const projectId = import.meta.env.VITE_WALLETCONNECT_PROJECT_ID || 'f55ba487b823b0308e4621a87d4ebf76';
+const projectId = '50d2e6a49b5c231708de8e982bf538d5';
 
 const metadata = {
     name: 'FundTracer',
@@ -28,11 +28,11 @@ createAppKit({
         email: false,
         socials: [],
     },
+    allWallets: 'SHOW',
     featuredWalletIds: [
         'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
         'a797aa35c0fadbfc1a53e7f675162ed5226968b44a19ee3d24385c64d1d3c393',
         '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0',
-        'fd20dc426fb37566d803205b19bbc1d4096b248ac04548e3cfb6b3a38bd033aa',
     ],
     enableInjected: true,
     enableEIP6963: true,
